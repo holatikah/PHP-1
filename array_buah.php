6b006cf192975c1d55f29ef574ffa1d544c80e59
holatikah <92282151+holatikah@users.noreply.github.com>
2022-03-03 18:02:38 +0700

    Add files via upload

---
+++ array_buah.php
@@ -0,0 +1,21 @@
+<?php
+    $ar_buah = ["Pepaya","Mangga","Pisang","Jambu"];
+    // cetak buah ke index ke-2
+    echo $ar_buah[2];
+    //cetak jumlah buah
+    echo '<br/>Jumlah Buah ' .count($ar_buah);
+    //cetak seluruh buah
+    echo '</ol>';
+    //tambahkan buah
+    $ar_buah[]="Durian";
+    //hapus buah index ke-1
+    unset($ar_buah[1]);
+    //ubah buah index ke-2 jadi Manggis
+    $ar_buah[2]="Manggis";
+    //cetak seluruh buah dengan indexnya
+    echo '<ul>';
+    foreach($ar_buah as $k => $v){
+        echo '<li> buah index - ' . $k . ' adalah '. $v .'</li>';    
+    }
+    echo '</ul>';
+?>
---
+++ variable_user.php
@@ -0,0 +1,13 @@
+<?php
+    $nama = 'Atikah Khairun Nisa';
+    $umur = 19;
+    $tb = 156;
+    $bb = 55;
+
+    echo 'Nama : ' .$nama;
+    echo '<br/>Umur : ' .$umur.' Tahun';
+    echo '<br/>Tinggi Badan : ' .$tb.' Cm';
+    echo '<br/>Berat Badan : ' .$bb.' Kg';
+
+    echo "<br/>Hello $nama Apa Kabar?";
+?>
